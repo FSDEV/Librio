29176891556ce56f4e2b31663103b4befec86e24
Chris Miller <lordsauronthegreat@gmail.com>
2011-03-10 07:38:59 -0800

    initial commit

---
+++ footer.php
@@ -0,0 +1,12 @@
+	</div>
+	
+	<div id="footer">
+		<p>
+			Copyright &copy; 2008. All right reserved. 
+			Theme by <a href="http://deniart.ru">deniart</a>, modified by fsdev-cmiller.
+		</p>
+	</div>
+</div>
+
+</body>
+</html>
